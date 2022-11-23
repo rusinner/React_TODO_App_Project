e52442e0d4047b9ccdb983f2bd03f1e615e2e4f5
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-23 21:31:37 +0200

    render todos on submit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+//import components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header>
+        <h1>TODO List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import component
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => {
+          return <Todo text={todo.text} key={todo.id} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
